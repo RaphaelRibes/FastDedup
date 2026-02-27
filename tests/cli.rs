21233938570199d473dc124e0a8b79a2940f905c
raphael <raphael.ribes@gmail.com>
2026-02-27 09:04:03 +0100

    linted

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,7 +1,6 @@
-use assert_cmd::Command;
+use assert_cmd::cargo;
 use assert_fs::prelude::*;
 use predicates::prelude::*;
-use std::io::Write;
 
 fn generer_fastq(
     dir: &assert_fs::TempDir,
@@ -39,7 +38,7 @@ fn generer_fastq(
 #[test]
 fn test_fichier_entree_inexistant() {
     let temp_dir = assert_fs::TempDir::new().unwrap();
-    let mut cmd = Command::cargo_bin("fdedup").unwrap();
+    let mut cmd = cargo::cargo_bin_cmd!("fdedup");
     cmd.current_dir(temp_dir.path())
         .arg("fichier_fantome.fastq")
         .assert()
@@ -55,7 +54,7 @@ fn test_fichier_entree_vide() {
     let fichier_vide = temp_dir.child("vide.fastq");
     fichier_vide.touch().unwrap();
 
-    let mut cmd = Command::cargo_bin("fdedup").unwrap();
+    let mut cmd = cargo::cargo_bin_cmd!("fdedup");
     cmd.arg(fichier_vide.path())
         .arg("-s") // simulation
         .assert()
@@ -75,7 +74,7 @@ fn test_fichier_entree_mauvais_format() {
         )
         .unwrap();
 
-    let mut cmd = Command::cargo_bin("fdedup").unwrap();
+    let mut cmd = cargo::cargo_bin_cmd!("fdedup");
     cmd.arg(fichier_texte.path())
         .arg("-s")
         .assert()
@@ -91,7 +90,7 @@ fn test_fichier_sortie_sans_permissions_ou_introuvable() {
     let temp_dir = assert_fs::TempDir::new().unwrap();
     let fichier_entree = generer_fastq(&temp_dir, "entree.fastq", 10, 0);
 
-    let mut cmd = Command::cargo_bin("fdedup").unwrap();
+    let mut cmd = cargo::cargo_bin_cmd!("fdedup");
     cmd.arg(fichier_entree.path())
         .arg("/chemin/vers/un/dossier/qui/n/existe/pas/sortie.fastq")
         .assert()
@@ -108,7 +107,7 @@ fn test_fichier_sortie_gz_corrompu() {
         .write_str("Ceci n'est pas un fichier compressé gzip valide.")
         .unwrap();
 
-    let mut cmd = Command::cargo_bin("fdedup").unwrap();
+    let mut cmd = cargo::cargo_bin_cmd!("fdedup");
     cmd.arg(fichier_entree.path())
         .arg(fichier_sortie_corrompu.path())
         .assert()
@@ -124,7 +123,7 @@ fn test_taux_duplication_10_et_50_pourcents() {
 
     // Test 100 séquences uniques
     let seq_100_uniques = generer_fastq(&temp_dir, "100_uniques.fastq", 100, 0);
-    let mut cmd = Command::cargo_bin("fdedup").unwrap();
+    let mut cmd = cargo::cargo_bin_cmd!("fdedup");
     cmd.arg(seq_100_uniques.path())
         .arg("-s")
         .arg("-v")
@@ -135,7 +134,7 @@ fn test_taux_duplication_10_et_50_pourcents() {
 
     // Test 110 séquences (100 uniques + 10 dupliquées)
     let seq_110 = generer_fastq(&temp_dir, "110_seq.fastq", 100, 10);
-    let mut cmd2 = Command::cargo_bin("fdedup").unwrap();
+    let mut cmd2 = cargo::cargo_bin_cmd!("fdedup");
     cmd2.arg(seq_110.path())
         .arg("-s")
         .arg("-v")
@@ -146,7 +145,7 @@ fn test_taux_duplication_10_et_50_pourcents() {
 
     // Test 150 séquences (100 uniques + 50 dupliquées)
     let seq_150 = generer_fastq(&temp_dir, "150_seq.fastq", 100, 50);
-    let mut cmd3 = Command::cargo_bin("fdedup").unwrap();
+    let mut cmd3 = cargo::cargo_bin_cmd!("fdedup");
     cmd3.arg(seq_150.path())
         .arg("-s")
         .arg("-v")
